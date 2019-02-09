5e2143a8ec9f05a4f2e9726455d3465215712035
Daniel Kornas <harvi_pl@wp.pl>
2019-02-09 16:44:41 +0100

    Correct devtools code - for new devtools version, remove deprecated elements

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,4 @@
-import { createStore, compose } from 'redux';
+import { createStore } from 'redux';
 import reducers from './reducers';
-const enhancers = compose(
-    window.devToolsExtension ? window.devToolsExtension() : f => f
-  );
-export const store = createStore(reducers, enhancers);
+
+export const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
